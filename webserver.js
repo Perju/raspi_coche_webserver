4bf7e8eeca344af1c9d740f61c2d996fca455a55
Juan Pablo Pérez García <yohanam.paulus@gmail.com>
2019-12-06 14:09:17 +0000

    Refactorización legilibilidad del código.

--- webserver.js
+++ webserver.js
@@ -1,4 +1,3 @@
-
 var Raspiloto = new (require('./raspiloto')).Raspiloto();
 
 //var Raspiloto = new rpi.Raspiloto();
@@ -11,60 +10,60 @@ http.listen(8080,function(){
 });
 
 function handler(req, res){
-  fs.readFile(__dirname+'/client.html', function(err,data){
-    if(err){
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }else{
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      return res.end();
-    }
-  });
+    fs.readFile(__dirname+'/client.html', function(err,data){
+	if(err){
+	    res.writeHead(404, {'Content-Type': 'text/html'});
+	    return res.end("404 Not Found");
+	}else{
+	    res.writeHead(200, {'Content-Type': 'text/html'});
+	    res.write(data);
+	    return res.end();
+	}
+    });
 }
 
 io.sockets.on('connection', function (socket) {// WebSocket Connection
-  console.log('Usuario conectado');
-
-  socket.send('Bienvenido');
-
-  socket.on('pruebas', function(data){
-    console.log('pruebas');
-    var datos = "";
-    for(var i=0;i<data.length;i++){
-      datos += data[i];
-    }
-    console.log(datos);
-  });
-
-  socket.on('luces', function(data){
-    console.log("evento luces: "+data);
-    Raspiloto.cambia_estado_luces(data);
-  });
-
-  socket.on('intermitentes', function(data){
-    console.log("evento intermitentes: "+data);
-    Raspiloto.cambia_estado_intermitentes(data);
-  });
-
-  socket.on('emergencias', function(data){
-    console.log("evento emergencias: "+data);
-    Raspiloto.cambia_estado_emergencias(data);
-  });
-
-  socket.on('claxon', function(data){
-    console.log("Evento claxon: "+data);
-    Raspiloto.cambia_estado_claxon(data);
-  });
-
-  socket.on('movimiento', function(data){
-    console.log("evento movimiento: "+data);
-    Raspiloto.cambia_estado_movimiento(data);
-  });
-
-  socket.on('disconnect', function(data){
-    console.log('conexion cerrada');
-    Raspiloto.desactivar_sistemas();
-  });
-
+    console.log('Usuario conectado');
+    
+    socket.send('Bienvenido');
+    
+    socket.on('pruebas', function(data){
+	console.log('pruebas');
+	var datos = "";
+	for(var i = 0; i < data.length; i++){
+	    datos += data[i];
+	}
+	console.log(datos);
+    });
+    
+    socket.on('luces', function(data){
+	console.log("evento luces: "+data);
+	Raspiloto.cambia_estado_luces(data);
+    });
+    
+    socket.on('intermitentes', function(data){
+	console.log("evento intermitentes: "+data);
+	Raspiloto.cambia_estado_intermitentes(data);
+    });
+    
+    socket.on('emergencias', function(data){
+	console.log("evento emergencias: "+data);
+	Raspiloto.cambia_estado_emergencias(data);
+    });
+    
+    socket.on('claxon', function(data){
+	console.log("Evento claxon: "+data);
+	Raspiloto.cambia_estado_claxon(data);
+    });
+    
+    socket.on('movimiento', function(data){
+	console.log("evento movimiento: "+data);
+	Raspiloto.cambia_estado_movimiento(data);
+    });
+    
+    socket.on('disconnect', function(data){
+	console.log('conexion cerrada');
+	Raspiloto.desactivar_sistemas();
+    });
+    
 });
