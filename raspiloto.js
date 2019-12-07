ca2b36443f42819187e887698846b1417e8a2203
Juan Pablo Pérez García <yohanam.paulus@gmail.com>
2019-12-07 17:07:31 +0000

    Controlado por estados, hay que llamar al metodo nuevo

--- raspiloto.js
+++ raspiloto.js
@@ -4,10 +4,14 @@ class Raspiloto {
     
     constructor(){
 	// estructura de datos para gestionar los eventos de forma mas avanzada
-	this.status={'luces':'off',
-		     'intermitentes':'off',
-		     'emergencias':'off',
-		     'movimiento':'reposo'};
+	this.currentStatus={'luces':'off',
+			    'intermitentes':'reposo',
+			    'emergencias':'off',
+			    'movimiento':'reposo'};
+	this.newStatus={'luces':'off',
+			    'intermitentes':'reposo',
+			    'emergencias':'off',
+			    'movimiento':'reposo'};
 	// pines para las luces y los intermitentes
 	// TODO cambiar todos los numeros de los pines, no se pude congiurar un GND como salida.
 	this.luces = HwConfigurador.preparePINs([22,23,24,25],'out');
@@ -22,58 +26,82 @@ class Raspiloto {
 	//buzzer para el claxon
 	this.claxon = HwConfigurador.preparePINs([4],'out');
     }
-    
-    cambia_estado_luces(estado){
-	if(estado=="on"){
-            HwConfigurador.switchOnPins(this.luces);
-	}else if(estado=="off"){
-            HwConfigurador.switchOffPins(this.luces);
+
+    updateState(){
+	// Estado de las luces
+	if(this.newStatus.luces!=this.currentStatus.luces){
+	    if(this.newStatus.luces=="on"){
+		HwConfigurador.switchOnPins(this.luces);
+	    }else if(this.newStatus.luces=="off"){
+		HwConfigurador.switchOffPins(this.luces);
+	    }
 	}
+	// Estado de los intermitenes
+	if(this.newStatus.intermitentes!=this.currentStatus.intermitentes){
+	    if(this.newStatus.intermitentes=="Derecho"){
+		HwConfigurador.stopBlink(this.intermitentesIzq, this.blinkInterval);
+		HwConfigurador.startBlink(this.intermitentesDer, this.blinkInterval);
+	    }else if(this.newStatus.intermitentes=="Izquierdo"){
+		HwConfigurador.stopBlink(this.intermitentesDer, this.blinkInterval);
+		HwConfigurador.startBlink(this.intermitentesIzq, this.blinkInterval);
+	    }else if(this.newStatus.intermitentes=="Reposo"){
+		HwConfigurador.stopBlink(this.intermitentesDer, this.blinkInterval);
+		HwConfigurador.stopBlink(this.intermitentesIzq, this.blinkInterval);
+	    }
+	}
+	// Estado de las luces de emergencia
+	if(this.newStatus.emergencias!=this.currentStatus.emergencias){
+	    if(newStatus.emergencias=="on"){
+		HwConfigurador.startBlink(this.intermitentesDer.concat(this.intermitentesIzq), this.blinkInterval);
+	    }else if(estado=="off"){
+		HwConfigurador.stopBlink(this.intermitentesDer.concat(this.intermitentesIzq), this.blinkInterval);
+	    }
+	}
+	// Estado de los motores
+	if(this.newStatus.movimiento!=this.currentStatus.emergencias){
+	    HwConfigurador.switchOffPins(this.motorIzq.concat(this.motorDer));
+	    if(this.newStatus.movimiento=="Palante"){
+		HwConfigurador.switchOnPins([this.motorIzq[0],this.motorDer[0]]);
+	    }else if(this.newStatus.movimiento=="Patras"){
+		HwConfigurador.switchOnPins([this.motorIzq[1],this.motorDer[1]]);
+	    }else if(this.newStatus.movimiento=="Derecha"){
+		HwConfigurador.switchOnPins([this.motorIzq[0],this.motorDer[1]]);
+	    }else if(this.newStatus.movimiento=="Izquierda"){
+		HwConfigurador.switchOnPins([this.motorIzq[1],this.motorDer[0]]);
+	    }else if(this.newStatus.movimiento=="Parar"){
+		// Se para en caso de indicar algun movimiento
+	    }
+	}
+	// Estado del claxon
+	if(this.newStatus.claxon!=this.currentStatus.claxon){
+	    if(this.newStatus.claxon==true){
+		HwConfigurador.switchOnPins(this.claxon);
+	    }else if(this.newStatus==false){
+		HwConfigurador.switchOffPins(this.claxon);
+	    }	    
+	}
+	this.currentStatus=this.newStatus.s;
     }
     
-    cambia_estado_intermitentes(estado){
-	console.log("cambia estado intermitentes: "+estado);
-      if(estado=="Derecho"){
-	  HwConfigurador.stopBlink(this.intermitentesIzq, this.blinkInterval);
-	  HwConfigurador.startBlink(this.intermitentesDer, this.blinkInterval);
-      }else if(estado=="Izquierdo"){
-	  HwConfigurador.stopBlink(this.intermitentesDer, this.blinkInterval);
-	  HwConfigurador.startBlink(this.intermitentesIzq, this.blinkInterval);
-      }else if(estado=="Reposo"){
-	  HwConfigurador.stopBlink(this.intermitentesDer, this.blinkInterval);
-	  HwConfigurador.stopBlink(this.intermitentesIzq, this.blinkInterval);
-      }
+    cambia_estado_luces(isOn){
+	this.newStatus.luces=isOn;
     }
     
-    cambia_estado_emergencias(estado){
-	if(estado=="on"){
-	    HwConfigurador.startBlink(this.intermitentesDer.concat(this.intermitentesIzq), this.blinkInterval);
-	}else if(estado=="off"){
-	    HwConfigurador.stopBlink(this.intermitentesDer.concat(this.intermitentesIzq), this.blinkInterval);
-	}
+    cambia_estado_intermitentes(strIntermitentes){
+	console.log("Param estado: "+strIntermitentes);
+	this.newStatus.intermitentes=strIntermitentes;
     }
     
-    cambia_estado_movimiento(estado){
-	HwConfigurador.switchOffPins(this.motorIzq.concat(this.motorDer));
-	if(estado=="Palante"){
-	    HwConfigurador.switchOnPins([this.motorIzq[0],this.motorDer[0]]);
-	}else if(estado=="Patras"){
-	    HwConfigurador.switchOnPins([this.motorIzq[1],this.motorDer[1]]);
-	}else if(estado=="Derecha"){
-	    HwConfigurador.switchOnPins([this.motorIzq[0],this.motorDer[1]]);
-	}else if(estado=="Izquierda"){
-	    HwConfigurador.switchOnPins([this.motorIzq[1],this.motorDer[0]]);
-	}else if(estado=="Parar"){
-	    console.log("no hago nada porque ya esta todo parado");
-	}
+    cambia_estado_emergencias(isOn){
+	this.newStatus.emergencias=isOn;
     }
     
-    cambia_estado_claxon(estado){
-	if(estado==true){
-	    HwConfigurador.switchOnPins(this.claxon);
-	}else if(estado==false){
-	    HwConfigurador.switchOffPins(this.claxon);
-	}
+    cambia_estado_movimiento(strMovimiento){
+	this.newStatus.movimiento=strMovimiento;
+    }
+    
+    cambia_estado_claxon(isOn){
+	this.newStatus.claxon=isOn;
     }
     
     desactivar_sistemas(){
